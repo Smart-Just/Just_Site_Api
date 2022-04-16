ae25fd12ca321a8ca01882ee9d25ad834c311140
nGyu <vpdls1511@gmail.com>
2022-04-16 16:08:17 +0900

    Intial_Commit & Push

---
+++ src/main/java/com/example/just_site_api/JustSiteApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.just_site_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustSiteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustSiteApiApplication.class, args);
+    }
+
+}
